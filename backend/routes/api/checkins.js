108d985c247528a1f232cef38bb640b4ed235992
Nick Rogers <nickjrogers@gmail.com>
2021-11-11 17:00:44 -0800

    checkins update functional

--- backend/routes/api/checkins.js
+++ backend/routes/api/checkins.js
@@ -16,6 +16,22 @@ router.post('/', requireAuth, asyncHandler(async (req, res) => {
   return res.json({ checkin });
 }));
 
+router.put('/:id(\\d+)', requireAuth, asyncHandler(async (req, res) => {
+  const id = req.params.id;
+  const { comment, wineryId, wineId } = req.body;
+
+  const checkin = await Checkin.findByPk(id);
+  await checkin.update({
+    comment,
+    wineryId,
+    wineId
+  });
+  if (checkin) {
+    await checkin.save();
+  }
+  return res.json({ checkin });
+}))
+
 
 
 module.exports = router;
--- frontend/src/store/checkins.js
+++ frontend/src/store/checkins.js
@@ -2,7 +2,8 @@ import { csrfFetch } from "./csrf";
 
 const ADD_CHECKIN = 'checkins/addCheckin';
 const LOAD_CHECKINS = 'checkins/loadCheckins';
-const REMOVE_CHECKINS = 'checkins/removeCheckins'
+const REMOVE_CHECKINS = 'checkins/removeCheckins';
+const EDIT_CHECKIN = 'checkins/editCheckins';
 
 const addCheckin = (payload) => {
   return {
@@ -15,6 +16,13 @@ const loadCheckins = (checkins) => {
   return {
     type: LOAD_CHECKINS,
     checkins
+  };
+};
+
+const editCheckin = (checkin) => {
+  return {
+    type: EDIT_CHECKIN,
+    checkin
   };
 };
 
@@ -38,7 +46,16 @@ export const getCheckins = (id) => async (dispatch) => {
   const response = await csrfFetch(`/api/users/${id}`);
   const data = await response.json();
   dispatch(loadCheckins(data));
-  console.log(data);
+  return data;
+}
+
+export const updateCheckin = (id, input) => async (dispatch) => {
+  const response = await csrfFetch(`/api/checkins/${id}`, {
+    method: 'PUT',
+    body: JSON.stringify(input)
+  });
+  const data = await response.json();
+  dispatch(editCheckin(data));
   return data;
 }
 
@@ -64,6 +81,10 @@ const checkinsReducer = (state = {}, action) => {
     case REMOVE_CHECKINS:
       newState = null;
       return newState
+    case EDIT_CHECKIN:
+      newState = { ...state }
+      newState[action.checkin.checkin.id] = action.checkin.checkin;
+      return newState;
     default:
       return state;
     };
