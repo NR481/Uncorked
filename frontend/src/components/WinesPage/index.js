cd9089ef9c0f1905d1cf31897688a975f8c5e661
Nick Rogers <nickjrogers@gmail.com>
2021-12-06 21:05:36 -0800

    styling wines page

--- frontend/src/components/WinesPage/index.js
+++ frontend/src/components/WinesPage/index.js
@@ -51,10 +51,17 @@ const WinesPage = ({ isLoaded }) => {
   };
 
   return (
-    <div>
-      <h1>Wines</h1>
-      <h2>Can't find the wine you're looking for?</h2>
-      <button onClick={revealWineForm}>Add A New One</button>
+    <div className='wines-container'>
+      <img
+        className='wine-img'
+        src='https://images.unsplash.com/photo-1535208239377-4a7f8e310dc5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8c29tbWVsaWVyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=400&q=60'
+        alt='vineyard'
+      />
+      <h1 className='wine-banner'>Wine List</h1>
+      <div className='new-wine'>
+        <h2>Can't find the wine you're looking for?</h2>
+        <button onClick={revealWineForm}>Add A New One</button>
+      </div>
       {addWineForm &&
         <form onSubmit={handleSubmit}>
           <label>
