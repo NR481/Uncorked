5e8116a776a95ba8be995bf5583f325e73249c6c
Nick Rogers <nickjrogers@gmail.com>
2022-02-22 10:38:48 -0800

    styling single wine page

--- frontend/src/components/SingleWine/CheckinModal.js
+++ frontend/src/components/SingleWine/CheckinModal.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { Modal } from "../context/Modal";
 import CheckinForm from "./CheckinForm";
+import "./SingleWine.css";
 
 const CheckinModal = ({ user, wine, winery }) => {
   const [modal, setModal] = useState(false);
@@ -9,6 +10,7 @@ const CheckinModal = ({ user, wine, winery }) => {
     <div>
       <button
         onClick={() => setModal(true)}
+        className="checkin-modal-button"
       >
         Check In
       </button>
